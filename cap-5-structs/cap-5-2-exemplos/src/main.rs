bedd63430e03933307e971389e86e447ca12f17c
fabio21777 <fabiosouza21777@gmail.com>
2024-07-14 10:28:52 -0300

    6.3 enun e ifs

--- cap-5-structs/cap-5-2-exemplos/src/main.rs
+++ cap-5-structs/cap-5-2-exemplos/src/main.rs
@@ -19,8 +19,21 @@ fn main() {
         height: 40,
     };
 
-    println!(" A area do retângulo é: {}", area(rectangle));
+    //println!(" A area do retângulo é: {}", area(rectangle));
     println!(" A area do retângulo é: {}", area_ref(&rect2));
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        teste => move_player(teste),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {
+        println!("num_spaces é {}", num_spaces)
+    }
 
     //println!("rect1 altura é {}", rect1.height); // isso da erro, pos o valor de rect1 foi movido para a função area
     println!("rect2 altura é {}", rect2.height); // isso da certo, pois passamos a referencia de rect2 para a função areaRef
