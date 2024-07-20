aa85062a5a796e7e621da85dfe831518c0c685a7
fabio21777 <fabiosouza21777@gmail.com>
2024-07-20 10:53:24 -0300

    7.2 e 7.3

---
+++ cap-7-pacotes-mods/restaurant/src/lib.rs
@@ -0,0 +1,19 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
